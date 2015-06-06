cedd6a82a466310f7f145951575c425115a64469
Jeremy Gunter <gunterja@gmail.com>
2015-06-06 13:59:50 -0500

    Trial run

--- calc.rb
+++ calc.rb
@@ -1,12 +1,12 @@
-# puts 1.0 + 2.0
-# puts 2.0 * 3.0
-# puts 5.0 - 8.0
-# puts 9.0 / 2.0
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
 
-# puts 365 * 24
-# puts 10 * (365 * 24 * 60)
-# puts 30.95 * (365 * 24 * 60 * 60)
-# puts ((((1204000000 / 60) / 60) /24) /365)
+puts 365 * 24
+puts 10 * (365 * 24 * 60)
+puts 30.95 * (365 * 24 * 60 * 60)
+puts ((((1204000000 / 60) / 60) /24) /365)
 
 puts "What is your first number?"
 input1 = gets.chomp
@@ -16,3 +16,4 @@ operand = gets.chomp
 
 puts "What is your second number?"
 input2 = gets.chomp
+
